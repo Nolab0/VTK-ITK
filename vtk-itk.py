210de2a7ae80c07694ef718afadc5d6c7137040e
noclone <noclonepvp@gmail.com>
2023-07-16 17:06:04 +0200

    ajout de visualisation 3D fonctionnelle (ENFIN)

--- vtk-itk.py
+++ vtk-itk.py
@@ -53,7 +53,9 @@ def Recalage(fixed, moving):
     cast_filter.Update()
     return cast_filter.GetOutput()
 
+print("Recalage en cours...")
 recalee = Recalage("Data/case6_gre1.nrrd", "Data/case6_gre2.nrrd")
+print("Recalage terminé !")
 
 def segmentation(img, seedX, seedY, seedZ, lower, upper):
     type_ = type(img)
@@ -85,8 +87,10 @@ def segmentation(img, seedX, seedY, seedZ, lower, upper):
 
     return out
 
+print("Segmentation en cours...")
 segmented_before = segmentation(image, 125, 65, 80, 600, 800)
 segmented_after = segmentation(recalee, 125, 65, 80, 600, 800)
+print("Segmentation terminée !")
 
 def compute_tumor_volume(image):
     image_type = type(image)
@@ -250,3 +254,120 @@ del renderer1
 del renderer2
 del window
 del interactor
+
+class CustomInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, mapper1, mapper2, actor1, actor2):
+        self.mapper1 = mapper1
+        self.mapper2 = mapper2
+        self.actor1 = actor1
+        self.actor2 = actor2
+        self.current_mapper = self.mapper1
+        self.AddObserver("KeyPressEvent", self.onKeyPress)
+        self.actor1.VisibilityOff()
+        self.actor2.VisibilityOff()
+
+    def onKeyPress(self, obj, event):
+        key = self.GetInteractor().GetKeySym()
+        # Show the tumor before
+        if key == "b":
+            if self.current_mapper == self.mapper1:
+                self.current_mapper = self.mapper2
+                self.actor2.VisibilityOn()
+            else:
+                self.current_mapper = self.mapper1
+                self.actor2.VisibilityOff()
+            self.GetInteractor().Render()
+        # Show the tumor after
+        if key == "a":
+            if self.current_mapper == self.mapper1:
+                self.current_mapper = self.mapper2
+                self.actor1.VisibilityOn()
+            else:
+                self.current_mapper = self.mapper1
+                self.actor1.VisibilityOff()
+            self.GetInteractor().Render()
+
+head_scan = itk.vtk_image_from_image(itk.imread('Data/case6_gre1.nrrd'))
+tumor_1_scan = itk.vtk_image_from_image(itk.imread('Data/segmented.nrrd'))
+tumor_2_scan = itk.vtk_image_from_image(itk.imread('Data/segmented2.nrrd'))
+
+# Create volume properties and mappers
+mapper2 = vtk.vtkSmartVolumeMapper()
+mapper2.SetInputData(head_scan)
+
+opacity_transfer_function = vtk.vtkPiecewiseFunction()
+opacity_transfer_function.AddPoint(0.0, 0.0)
+opacity_transfer_function.AddPoint(90.0, 0.0)
+opacity_transfer_function.AddPoint(100.0, 0.2)
+opacity_transfer_function.AddPoint(120.0, 0.0)
+
+colorFun = vtk.vtkColorTransferFunction()
+colorFun.AddRGBPoint(0, 1, 1, 1)
+
+volume_property = vtk.vtkVolumeProperty()
+volume_property.SetColor(colorFun)
+volume_property.SetScalarOpacity(opacity_transfer_function)
+
+# Create volume actor and set transforms
+volume = vtk.vtkVolume()
+volume.SetMapper(mapper2)
+volume.SetProperty(volume_property)
+
+matrix = head_scan.GetPhysicalToIndexMatrix()
+transf = vtk.vtkTransform()
+transf.Translate(itk.dict_from_image(itk.imread('Data/case6_gre1.nrrd'))["origin"])
+transf.PostMultiply()
+transf.Concatenate(volume.GetMatrix())
+volume.SetUserTransform(transf)
+
+transf = vtk.vtkTransform()
+transf.SetMatrix(matrix)
+transf.PostMultiply()
+transf.Concatenate(volume.GetMatrix())
+volume.SetUserTransform(transf)
+
+# Create contour filters, mappers, and actors for img[1] and img[2]
+tumor1 = vtk.vtkContourFilter()
+tumor1.SetInputData(tumor_1_scan)
+tumor1.GenerateValues(1, 135, 135)
+
+tumor2 = vtk.vtkContourFilter()
+tumor2.SetInputData(tumor_2_scan)
+tumor2.GenerateValues(1, 135, 135)
+
+mapper1 = vtk.vtkDataSetMapper()
+mapper1.SetInputConnection(tumor1.GetOutputPort())
+mapper1.SetScalarVisibility(False)
+
+actor1 = vtk.vtkActor()
+actor1.SetMapper(mapper1)
+actor1.GetProperty().SetColor(1, 0.5, 0)
+
+mapper2 = vtk.vtkDataSetMapper()
+mapper2.SetInputConnection(tumor2.GetOutputPort())
+mapper2.SetScalarVisibility(False)
+
+actor2 = vtk.vtkActor()
+actor2.SetMapper(mapper2)
+actor2.GetProperty().SetColor(1, 0, 0)
+actor2.VisibilityOff()
+
+# Create the renderer, render window, and interactor
+renderer = vtk.vtkRenderer()
+renderer.AddActor(actor1)
+renderer.AddActor(actor2)
+renderer.AddVolume(volume)
+
+render_window = vtk.vtkRenderWindow()
+render_window.AddRenderer(renderer)
+render_window.SetSize(800, 600)
+render_window.Render()
+
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(render_window)
+
+# Set the custom interactor style
+custom_style = CustomInteractorStyle(mapper1, mapper2, actor1, actor2)
+interactor.SetInteractorStyle(custom_style)
+
+interactor.Start()
